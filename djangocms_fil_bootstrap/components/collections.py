03006d2124f6d77b3f7b82e5d8b7e2f339396fb9
Noel da Costa <noeldacosta@gmail.com>
2019-02-26 12:27:58 +0000

    Update collections.py
    Fix: page was being accessed incorrectly – it would work in the app but not for unit testing becase the bootstrap object is mocked in tests and thus lacks the `get` method on it's `data` attribute

--- djangocms_fil_bootstrap/components/collections.py
+++ djangocms_fil_bootstrap/components/collections.py
@@ -13,11 +13,14 @@ class Collections(Component):
             collection_data = {
                 "author": self.bootstrap.users[data["user"]],
                 "workflow": self.bootstrap.workflows[data["workflow"]],
-                "name": name,
+                "name": data["name"],
             }
             collection = self.get_or_create(collection_data)
             for page in data.get("pages", []):
-                self.add_version(collection, self.get_version(self.bootstrap.pages.data.get(page)))
+                # access pages from bootstrap as if a dict because the __getitem__ method in Bootstrap
+                # allows this and without it mock objects will fail via other access methods.
+                self.add_version(collection, self.get_version(self.bootstrap.pages[page]))
+            self.data[name] = collection
 
     def get_version(self, page):
         return get_version(page)
