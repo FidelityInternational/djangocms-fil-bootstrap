1056f86c01e1a2b90579e03747bcb70d3c278e12
Noel da Costa <noeldacosta@gmail.com>
2019-02-26 12:28:25 +0000

    Update test_components.py
    Added tests for collections.
    Added comments about tests to help explain the general methodology.

--- tests/test_components.py
+++ tests/test_components.py
@@ -22,6 +22,7 @@ from djangocms_fil_bootstrap.utils import get_version
 from djangocms_moderation.models import ModerationCollection, Role, Workflow
 from djangocms_versioning.constants import DRAFT, PUBLISHED
 
+import sys
 
 class TestComponent(Component):
     field_name = "test"
@@ -32,22 +33,39 @@ class TestComponent(Component):
 
 class BaseTestCase(TestCase):
     def test_keeps_bootstrap(self):
+        """
+        Checks that if you pass the bootstrap component as a parameter, 
+        that it saves it as bootstrap attribute inside that component
+        Assertion tests that boostrap is accessible
+        """
         bootstrap = Mock()
         component = TestComponent(bootstrap)
         self.assertEqual(component.bootstrap, bootstrap)
 
     def test_call_keeps_raw_data(self):
+        """
+        Checks what happens when you call the object, 
+        that it saves the passed parameter as raw_data attribute inside that component
+        Assertion tests that boostrap is accessible
+        """
         component = TestComponent(Mock())
         component("foo")
         self.assertEqual(component.raw_data, "foo")
 
     def test_call_calls_parse(self):
+        """
+        Tests that parse is run when the component is called
+        """
         component = TestComponent(Mock())
         with patch.object(component, "parse") as parse:
             component("foo")
         parse.assert_called_once_with()
 
     def test_call_calls_default_factory_if_input_is_none(self):
+        """
+        If nothing is passed when calling the component, test that a default is created via a factory.
+        This prevents raw_data from having a None value. 
+        """
         factory = Mock(return_value="data from factory")
         FactoryTestComponent = type(
             "FactoryTestComponent", (TestComponent,), {"default_factory": factory}
@@ -58,11 +76,21 @@ class BaseTestCase(TestCase):
         self.assertEqual(component.raw_data, "data from factory")
 
     def test_getattr(self):
+        """
+        Tests magic method in base component. Component is expected to save it's data
+        in the data attribute (as a dict). Before parse() the dict is always empty.
+        The parse method adds to that dictionary via the c["a"] format.
+        """
         component = TestComponent(Mock())
         component.data = {"foo": "bar"}
         self.assertEqual(component["foo"], "bar")
 
     def test_getitem(self):
+        """
+        Tests magic method in base component. Component is expected to save it's data
+        in the data attribute (as a dict). Before parse() the dict is always empty.
+        The parse method adds to that dictionary via the c.a format.
+        """
         component = TestComponent(Mock())
         component.data = {"foo": "bar"}
         self.assertEqual(component.foo, "bar")
@@ -70,6 +98,12 @@ class BaseTestCase(TestCase):
 
 class UsersTestCase(TestCase):
     def test_parse(self):
+        """
+        Test the parse method directly (not implicitly), thus instead of component("foo"), 
+        set raw_data explicitly and call parse manually.
+        Checks that prepare_each is called using the raw_data value
+        Checks that each is called with the return value of prepare_each 
+        """
         component = Users(Mock())
         component.raw_data = ["foo"]
         with patch.object(component, "prepare_each") as prepare_each, patch.object(
@@ -80,6 +114,13 @@ class UsersTestCase(TestCase):
         each.assert_called_once_with(prepare_each.return_value)
 
     def test_each(self):
+        """
+        We want to know that each() actually calls the factory and that each stores the
+        result of that factory. We don't care what sort of data it is yet.
+        **data splits the data into arguments.
+        Check that a key is created with the username
+        Check that the value of that key matches.
+        """
         component = Users(Mock())
         data = {"username": "bar"}
         value = Mock(username="bar")
@@ -92,19 +133,34 @@ class UsersTestCase(TestCase):
         self.assertEqual(component.data["bar"], value)
 
     def test_prepare_each_only_username(self):
+        """
+        Check what data is prepared by prepare_earch function
+        Here it tests passing a string and fills in any blank expected keys.
+        return_value="domain" is because: here we are mocking the entire 
+        data function and always returns domain, so that we can know that 
+        the email data value will be and so that we can check that the data()
+        method is being used to get this value.
+        """
         bootstrap = Mock(data=Mock(return_value="domain"))
         component = Users(bootstrap)
         result = component.prepare_each("username")
+        bootstrap.data.assert_called_once_with("email_domain")
         self.assertEqual(result["username"], "username")
         self.assertEqual(result["email"], "username@domain")
         self.assertEqual(result["password"], "username")
         self.assertEqual(result["is_staff"], True)
         self.assertNotIn("foo", result)
 
+
     def test_prepare_each_dict(self):
+        """
+        Check what data is prepared by prepare_earch function
+        Here it tests passing a dictionary and fills in any blank expected keys.
+        """
         bootstrap = Mock(data=Mock(return_value="domain"))
         component = Users(bootstrap)
         result = component.prepare_each({"username": "username", "foo": "bar"})
+        bootstrap.data.assert_called_once_with("email_domain")
         self.assertEqual(result["username"], "username")
         self.assertEqual(result["email"], "username@domain")
         self.assertEqual(result["password"], "username")
@@ -114,6 +170,11 @@ class UsersTestCase(TestCase):
 
 class GroupsTestCase(TestCase):
     def test_add_user_to_group(self):
+        """
+        Mocking the whole bootstrap method, so we lose access to any stored components' methods
+        E.g. bootstrap.Users.get()
+        Tests that the django add() method is called with the correct arguement
+        """
         user = Mock()
         bootstrap = Mock(users={"bar": user})
         component = Groups(bootstrap)
@@ -481,14 +542,80 @@ class WorkflowsTestCase(TestCase):
 
 
 class CollectionsTestCase(TestCase):
+    def setUp(self):
+        self.user = UserFactory(username="user1")
+        self.wf1 = Workflow.objects.create(name="Workflow 1")
+        self.version = PageVersionFactory()
+        self.page1 = self.version.content.page
+        self.bootstrap = Mock(users={"user1": self.user}, workflows={"wf1": self.wf1}, pages={"page1": self.page1})
+        self.component = Collections(self.bootstrap)
+
     def test_parse(self):
-        user = UserFactory()
-        user.username = 'user1'
-        user.save()
-        user.refresh_from_db()
-        bootstrap = Mock(users={"user1": user}, workflows={"wf1": Workflow.objects.create(name="Workflow 1")})
-        component = Collections(bootstrap)
-        component.raw_data = {'collection1': {'pages': ['page1', 'page2'], 'name': 'Collection 1', 'user': 'user1', 'workflow': 'wf1'}}            
+        """
+        Test that parsing is able to call the Collection.get_or_create method
+        """
+        component = self.component
+        component.raw_data = {'collection1': {'pages': ['page1'], 'name': 'Collection 1', 'user': 'user1', 'workflow': 'wf1'}}            
+
+        with patch.object(
+            component, 'get_version', return_value=self.version
+        ) as get_version, patch.object(
+            component, 'get_or_create'
+        ) as get_or_create, patch.object(
+            component, 'add_version'
+        ) as add_version:
+            component.parse()
+            get_or_create.assert_called_once_with({'workflow': self.wf1, 'name': 'Collection 1', 'author': self.user})
+            
+    def test_get_version(self):
+        version = self.component.get_version(self.page1)
+        self.assertEqual(self.version, version)
+
+    def test_get_or_create(self):
+        # test add
+        collection_data = {
+            "author": self.user,
+            "workflow": self.wf1,
+            "name": 'Collection 1',
+        }
+        collection = self.component.get_or_create(collection_data)
+        self.assertEqual(collection.id, 1)
+        collection_data2 = {
+            "author": self.user,
+            "workflow": self.wf1,
+            "name": 'Collection 2',
+        }
+        collection2 = self.component.get_or_create(collection_data2)
+        self.assertEqual(collection2.id, 2)
+
+        # test get
+        collection3 = self.component.get_or_create(collection_data)
+        self.assertEqual(collection.id, 1)
+
+    def test_add_version(self):
+        """
+        Test that adding a version to a collection creates 
+        a moderation request which references that version
+        """
+        collection_data = {
+            "author": self.user,
+            "workflow": self.wf1,
+            "name": 'Collection 1',
+        }
+        collection = self.component.get_or_create(collection_data)
+        version = self.component.add_version(collection, self.version)
+        moderation_request = collection.moderation_requests.first()
+        stored_version = moderation_request.version
+        # check the stored version
+        self.assertEqual(self.version, stored_version) 
+        # check the stored content
+        self.assertEqual(moderation_request.version.content, self.version.content)
+        
+    def test_integration(self):
+        component = self.component
+        component.raw_data = {'collection1': {'pages': ['page1'], 'name': 'Collection 1', 'user': 'user1', 'workflow': 'wf1'}}            
         component.parse()
-        self.assertIn("foo", component.data)
-        self.assertEqual(component.data["foo"].name, "visible name")
+        self.assertEqual(ModerationCollection.objects.count(), 1)
+        collection = component.data["collection1"]
+        self.assertEqual(collection.name, "Collection 1")
+
