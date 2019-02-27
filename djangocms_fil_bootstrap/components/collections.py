816bed20db3548ee3d7d251d33cb812c60373264
Noel da Costa <noeldacosta@gmail.com>
2019-02-27 12:24:57 +0000

    Update collections.py
    Added docstring to explain assumptions about uniqueness for this component.

--- djangocms_fil_bootstrap/components/collections.py
+++ djangocms_fil_bootstrap/components/collections.py
@@ -9,6 +9,12 @@ class Collections(Component):
     default_factory = dict
 
     def parse(self):
+        """
+        ModerationCollections do not have unique constraints. However, for the purpose of 
+        importing initial data for a project that has djangocms-moderation installed
+        the working assumption is that a combination of the 3 keys: author, workflow and name
+        can be usefully considered to be a unique constraint.  
+        """
         for name, data in self.raw_data.items():
             collection_data = {
                 "author": self.bootstrap.users[data["user"]],
