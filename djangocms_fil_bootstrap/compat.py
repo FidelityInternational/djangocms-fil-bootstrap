b1d418090c5d3b184225e8db589520e6a853b5ce
Josh Peng Yu <joshyu@users.noreply.github.com>
2024-03-06 13:40:21 +0000

    fix: `packaging` not exist in python version less than 3.9

--- djangocms_fil_bootstrap/compat.py
+++ djangocms_fil_bootstrap/compat.py
@@ -1,6 +1,9 @@
 from cms import __version__ as cms_version
 
-from packaging.version import Version
+try:
+    from packaging.version import Version
+except ImportError:
+    from distutils.version import LooseVersion as Version
 
 
 DJANGO_CMS_4_1 = Version(cms_version) >= Version('4.1')
